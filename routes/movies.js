ac83d63b3aef71f37f7a9f35dcd8e175efca48ec
plaetzaw <plaetzaw@gmail.com>
2021-01-21 15:31:30 -0600

    Passing response as an array of objects, instead of needing multiple requests to server

--- routes/movies.js
+++ routes/movies.js
@@ -22,68 +22,27 @@ router.post("/popularMovies", async (req, res) => {
 })
 
 router.post("/fullmovieInfo", async (req, res) => {
+    console.log(req.body)
     let movie_id = req.body.id
     console.log(movie_id)
-    let fullURL = `https://api.themoviedb.org/3/movie/${movie_id}?api_key=${API_KEY}&language=en-US`
+    let fullInfoURL = `https://api.themoviedb.org/3/movie/${movie_id}?api_key=${API_KEY}&language=en-US`
+    let creditsURL = `https://api.themoviedb.org/3/movie/${movie_id}/credits?api_key=${API_KEY}&language=en-US`
+    let recsURL = `https://api.themoviedb.org/3/movie/${movie_id}/recommendations?api_key=${API_KEY}&language=en-US`
+    let providersURL = `https://api.themoviedb.org/3/movie/${movie_id}/watch/providers?api_key=${API_KEY}&language=en-US`
+
 
     try {
-        let fullMovie = await axios.get(fullURL)
-        res.status(200).send(fullMovie.data)
+        let movieinfo = []
+        let info = await axios.get(fullInfoURL);
+        let credits = await axios.get(creditsURL);
+        let recs = await axios.get(recsURL);
+        let providers = await axios.get(providersURL)
+        let fullmovieinfo = movieinfo.concat(info.data, credits.data, recs.data, providers.data)
+        res.status(200).send(fullmovieinfo);
     }  catch (e) {
-        res.status(500).json({ message: "An error has occured", error: e})
+        res.status(500).json({ message: "An error has occured", error: e});
     }
 })
 
-router.post("/fullmovieCredits", async (req, res) => {
-    let movie_id = req.body.id
-    console.log(movie_id)
-    let fullURL = `https://api.themoviedb.org/3/movie/${movie_id}/credits?api_key=${API_KEY}&language=en-US`
-
-    try {
-        let fullMovie = await axios.get(fullURL)
-        res.status(200).send(fullMovie.data)
-    }  catch (e) {
-        res.status(500).json({ message: "An error has occured", error: e})
-    }
-})
-
-router.post("/fullmovieRecommendations", async (req, res) => {
-    let movie_id = req.body.id
-    console.log(movie_id)
-    let fullURL = `https://api.themoviedb.org/3/movie/${movie_id}/recommendations?api_key=${API_KEY}&language=en-US`
-
-    try {
-        let fullMovie = await axios.get(fullURL)
-        res.status(200).send(fullMovie.data)
-    }  catch (e) {
-        res.status(500).json({ message: "An error has occured", error: e})
-    }
-})
-
-router.post("/fullmovieVideos", async (req, res) => {
-    let movie_id = req.body.id
-    console.log(movie_id)
-    let fullURL = `https://api.themoviedb.org/3/movie/${movie_id}/videos?api_key=${API_KEY}&language=en-US`
-
-    try {
-        let fullMovie = await axios.get(fullURL)
-        res.status(200).send(fullMovie.data)
-    }  catch (e) {
-        res.status(500).json({ message: "An error has occured", error: e})
-    }
-})
-
-router.post("/fullmovieWatchProviders", async (req, res) => {
-    let movie_id = req.body.id
-    console.log(movie_id)
-    let fullURL = `https://api.themoviedb.org/3/movie/${movie_id}/watch/providers?api_key=${API_KEY}&language=en-US`
-
-    try {
-        let fullMovie = await axios.get(fullURL)
-        res.status(200).send(fullMovie.data)
-    }  catch (e) {
-        res.status(500).json({ message: "An error has occured", error: e})
-    }
-})
 
 module.exports = router;
