f5c7755d94afd38e0a52a49788edc41a0d54743a
plaetzaw <plaetzaw@gmail.com>
2021-05-05 16:34:05 -0500

    Promise chain optimization

--- routes/people.js
+++ routes/people.js
@@ -22,19 +22,20 @@ router.post('/popularPeople', async (req, res) => {
 
 router.post('/personInfo', async (req, res) => {
   const personID = req.body.id
-  console.log(personID)
   const infoURL = `https://api.themoviedb.org/3/person/${personID}?api_key=${API_KEY}&language=en-US`
   const moviecreditsURL = `https://api.themoviedb.org/3/person/${personID}/movie_credits?api_key=${API_KEY}&language=en-US`
   const tvcreditsURL = `https://api.themoviedb.org/3/person/${personID}/tv_credits?api_key=${API_KEY}&language=en-US&`
 
   try {
-    const info = await axios.get(infoURL)
-    const moviecredits = await axios.get(moviecreditsURL)
-    const tvcredits = await axios.get(tvcreditsURL)
+    // const info = await axios.get(infoURL)
+    // const moviecredits = await axios.get(moviecreditsURL)
+    // const tvcredits = await axios.get(tvcreditsURL)
+
+    const promisechain = await Promise.all([axios.get(infoURL), axios.get(moviecreditsURL), axios.get(tvcreditsURL)])
     const personinfo = {
-      info: info.data,
-      moviecredits: moviecredits.data,
-      tvcredits: tvcredits.data
+      info: promisechain[0].data,
+      moviecredits: promisechain[1].data,
+      tvcredits: promisechain[2].data
     }
     res.status(200).send(personinfo)
   } catch (e) {
